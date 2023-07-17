ab160a30d76a067dacee1e23735e1c58295b9caa
Jason E. Church <elijahchurch@gmail.com>
2023-07-17 11:42:01 -0700

    add additional options to to Form.cshmtl and update other files to include it

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -24,11 +24,14 @@ namespace FriendLetter.Controllers
         public ActionResult Form() { return View(); }
 
         [Route("/postcard")]
-        public ActionResult Postcard(string recipient, string sender)
+        public ActionResult Postcard(string recipient, string sender, string place1, string place2, string souvenir)
         {
         LetterVariable myLetterVariable = new LetterVariable();
         myLetterVariable.Recipient = recipient;
         myLetterVariable.Sender = sender;
+        myLetterVariable.Place1 = place1;
+        myLetterVariable.Place2 = place2;
+        myLetterVariable.Souvenir = souvenir;
         return View(myLetterVariable);
         }
     }
--- FriendLetter/Models/LetterVariable.cs
+++ FriendLetter/Models/LetterVariable.cs
@@ -4,5 +4,8 @@ namespace FriendLetter.Models
     {
         public string Recipient { get; set; }
         public string Sender { get; set; }
+        public string Place1 { get; set;}
+        public string Place2 { get; set;}
+        public string Souvenir { get; set;}
     }
 }
